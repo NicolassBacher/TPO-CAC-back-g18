67aa08e4f52bc6332cda0246507b4800ddb6e3a3
vabrosnet <vabros@hotmail.com>
2024-07-06 22:56:52 -0300

    variables

--- db/db.js
+++ db/db.js
@@ -1,17 +1,27 @@
-const mysql = require('mysql2')
+// Importar el módulo dotenv para gestionar variables de entorno
+require('dotenv').config();
 
-const conection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'cac_viajes',
-})
+// Importar el módulo mysql2 para trabajar con MySQL
+const mysql = require('mysql2');
 
-conection.connect((error) => {
+// Configuración de la base de datos utilizando variables de entorno
+const config = {
+    host: process.env.DB_HOST || 'mysql-vabros.alwaysdata.net',
+    user: process.env.DB_USER || 'vabros',
+    password: process.env.DB_PASS || '2590Chue@',
+    database: process.env.DB_DATABASE || 'vabros_cac_viajes'
+};
+
+// Crear la conexión a la base de datos
+const connection = mysql.createConnection(config);
+
+// Conectar a la base de datos y manejar posibles errores
+connection.connect((error) => {
     if (error) {
-        return console.log(error)
+        return console.error('Error de conexión:', error);
     }
-    console.log('conectados')
-})
+    console.log('Conectados a la base de datos');
+});
 
-module.exports = conection
+// Exportar la conexión para usarla en otras partes de la aplicación
+module.exports = connection;
--- db/dbAsync.js
+++ db/dbAsync.js
@@ -1,12 +1,19 @@
+// Importar el módulo dotenv para gestionar variables de entorno
+require('dotenv').config();
+
+// Importar el módulo mysql2/promise para trabajar con MySQL
 const mysql = require('mysql2/promise');
 
+// Configuración de la base de datos utilizando variables de entorno
 const config = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'cac_viajes'
-}
+    host: process.env.DB_HOST || 'mysql-vabros.alwaysdata.net',
+    user: process.env.DB_USER || 'vabros',
+    password: process.env.DB_PASS || '2590Chue@',
+    database: process.env.DB_DATABASE || 'vabros_cac_viajes'
+};
 
-const pool = mysql.createPool(config)
+// Crear un pool de conexiones a la base de datos
+const pool = mysql.createPool(config);
 
-module.exports = pool
+// Exportar el pool de conexiones para usarlo en otras partes de la aplicación
+module.exports = pool;
